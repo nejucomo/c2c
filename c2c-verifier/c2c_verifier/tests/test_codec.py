52cfd97239d97d976c4312eace3e029c1ba045f1
Nathan Wilcox <nejucomo@gmail.com>
2014-09-12 19:18:23 -0700

    Begin specifying ``'a'`` format decoding.

--- c2c-verifier/c2c_verifier/tests/test_codec.py
+++ c2c-verifier/c2c_verifier/tests/test_codec.py
@@ -1,7 +1,7 @@
 import hashlib
 import unittest
 
-from c2c_verifier.codec import encode_a
+from c2c_verifier.codec import decode_a, encode_a
 
 
 class encode_a_tests (unittest.TestCase):
@@ -14,5 +14,19 @@ class encode_a_tests (unittest.TestCase):
         hashbits = hashlib.sha256('test input').digest()
         actual = encode_a(hashbits[:16])
         expected = 'auz9g6fqtip349nc88rhw9wtx4h'
+
+        self.assertEqual(expected, actual)
+
+
+class decode_a_tests (unittest.TestCase):
+
+    #@unittest.skip('Exposes zbase32 bug\n\n*** FIXME: File a bug with the upstream dependency.')
+    def test_decode_empty(self):
+        self.assertEqual('a', encode_a(''))
+
+    def test_decode_hash(self):
+        encstr = 'auz9g6fqtip349nc88rhw9wtx4h'
+        actual = decode_a(encstr)
+        expected = hashlib.sha256('test input').digest()
 
         self.assertEqual(expected, actual)
