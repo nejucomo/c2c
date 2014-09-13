8d1c09ccc4fbdd48ebfb44982bf8c4f2671095d7
Nathan Wilcox <nejucomo@gmail.com>
2014-09-12 18:51:50 -0700

    Fix the name of the test `test_encode_hash` and add `test_encode_empty` which exposes a `zbase32` bug (or undocumented precondition).

--- c2c-verifier/c2c_verifier/tests/test_encoder.py
+++ c2c-verifier/c2c_verifier/tests/test_encoder.py
@@ -5,7 +5,11 @@ from c2c_verifier.encoder import c2c_encode_a
 
 
 class c2c_encode_a_tests (unittest.TestCase):
-    def test_encode_null_hash(self):
+
+    def test_encode_empty(self):
+        self.assertEqual('a', c2c_encode_a(''))
+
+    def test_encode_hash(self):
         hashbits = hashlib.sha256('test input').digest()
         actual = c2c_encode_a(hashbits[:16])
         expected = 'auz9g6fqtip349nc88rhw9wtx4h'
