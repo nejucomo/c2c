1162a7d569a2a93dd465a907f1a00ca846af413c
Nathan Wilcox <nejucomo@gmail.com>
2014-09-12 18:57:04 -0700

    Rename some things `encoder` → `codec` and `c2c_encode_a` → `encode_a`.

--- c2c-verifier/c2c_verifier/codec.py
+++ c2c-verifier/c2c_verifier/codec.py
@@ -1,5 +1,5 @@
 import zbase32
 
 
-def c2c_encode_a(bits):
+def encode_a(bits):
     return 'a' + zbase32.b2a(bits)
--- c2c-verifier/c2c_verifier/tests/test_codec.py
+++ c2c-verifier/c2c_verifier/tests/test_codec.py
@@ -1,18 +1,18 @@
 import hashlib
 import unittest
 
-from c2c_verifier.encoder import c2c_encode_a
+from c2c_verifier.codec import encode_a
 
 
-class c2c_encode_a_tests (unittest.TestCase):
+class encode_a_tests (unittest.TestCase):
 
     @unittest.skip('Exposes zbase32 bug\n\n*** FIXME: File a bug with the upstream dependency.')
     def test_encode_empty(self):
-        self.assertEqual('a', c2c_encode_a(''))
+        self.assertEqual('a', encode_a(''))
 
     def test_encode_hash(self):
         hashbits = hashlib.sha256('test input').digest()
-        actual = c2c_encode_a(hashbits[:16])
+        actual = encode_a(hashbits[:16])
         expected = 'auz9g6fqtip349nc88rhw9wtx4h'
 
         self.assertEqual(expected, actual)
