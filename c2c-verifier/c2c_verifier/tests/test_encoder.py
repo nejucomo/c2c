4862629659925d80e79c40ea0218c835dce73173
Nathan Wilcox <nejucomo@gmail.com>
2014-09-12 18:50:43 -0700

    Fix a test bug for the encoder.

--- c2c-verifier/c2c_verifier/tests/test_encoder.py
+++ c2c-verifier/c2c_verifier/tests/test_encoder.py
@@ -7,7 +7,7 @@ from c2c_verifier.encoder import c2c_encode_a
 class c2c_encode_a_tests (unittest.TestCase):
     def test_encode_null_hash(self):
         hashbits = hashlib.sha256('test input').digest()
-        actual = c2c_encode_a(hashbits)
+        actual = c2c_encode_a(hashbits[:16])
         expected = 'auz9g6fqtip349nc88rhw9wtx4h'
 
         self.assertEqual(expected, actual)
